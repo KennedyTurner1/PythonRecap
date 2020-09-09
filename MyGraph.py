64efd04757208e640690b59036285e5df26933eb
Turner <Kennedy_Turner1@baylor.edu>
2020-09-09 17:00:03 -0500

    added print statement

--- MyGraph.py
+++ MyGraph.py
@@ -1,7 +1,9 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #missing libraries, we need to install it, separate from the root
 import numpy as np
 
 x = np.linspace(0,20,100)
 plt.plot(x, np.sin(x))
 
 plt.show()
+
+print("HELLO WORLD.")
